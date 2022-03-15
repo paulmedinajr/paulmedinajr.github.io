f932d3f39e8a3fac16b36502e677eb0097ddab86
Chris Blanquera <chris@incept.asia>
2022-03-15 19:59:59 +0800

    fixed buy box

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,7 +11,7 @@ async function deploy(name, ...params) {
 }
 
 const contractURI = 'https://ipfs.io/ipfs/bafkreicktn4jqck5ib4m2kv23h3md26ytgjyaoszeg5jup252j2daqrl5y'
-const tokenURI = 'https://www.polmedinajr.com/token/${id}.json'
+const tokenURI = 'https://www.polmedinajr.com/token'
 const recipients = [
   '0xc742466fd430c852aaA5bbC936933A080B2ad4aB', //pol
   '0x5CBFA0E4dE6934d3531D86F24a52e5e9Eb019786' //aod
