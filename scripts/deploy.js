bd3c7b06444d5d81055a5ae715f7d356c21abbd9
Chris Blanquera <chris@incept.asia>
2022-03-15 22:10:36 +0800

    whitelist

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,11 +10,11 @@ async function deploy(name, ...params) {
   return contract
 }
 
-const contractURI = 'https://ipfs.io/ipfs/bafkreicktn4jqck5ib4m2kv23h3md26ytgjyaoszeg5jup252j2daqrl5y'
+const contractURI = 'https://ipfs.io/ipfs/bafkreiel7ifk22wpw42lrpghar3h7nxqabychbupk5tjkltetqsciaouvu'
 const tokenURI = 'https://www.polmedinajr.com/token'
 const recipients = [
   '0xc742466fd430c852aaA5bbC936933A080B2ad4aB', //pol
-  '0x5CBFA0E4dE6934d3531D86F24a52e5e9Eb019786' //aod
+  '0x7B4d3367C87C85D150bc411B42fc8d9B1f5f4BB2' //aod
 ];
 const shares = [50, 50];
 
